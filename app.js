cdcb0c9b770105efd36d9d04458765d4c180e109
Timothy Stephens <info@tjcreadev.com>
2019-12-19 20:01:44 -0500

    Initial commits

---
+++ app.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const fs = require('fs');
+const chalk = require('chalk');
+
+const user = JSON.parse(fs.readFileSync('./user.json'));
+const userRequests = user.serviceRequests;
+const servReqs = JSON.parse(fs.readFileSync('./requests.json'));
+
+// List service requests associated with user
+const getRequests = (() => {
+    let gotRequests = [];
+    userRequests.forEach(userRequest => {        
+        let result = servReqs.find(request => {
+            return request.requestID === userRequest;
+        })
+        gotRequests.push(result.recover);
+    })
+    console.log(gotRequests);
+})
+
+getRequests();
