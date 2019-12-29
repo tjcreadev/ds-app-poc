d060805ed8b5b4c110466ea077a196005866f23b
Timothy Stephens <info@tjcreadev.com>
2019-12-29 00:24:25 -0500

    Fixed public directory location

--- utils/index.js
+++ utils/index.js
@@ -10,7 +10,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const path = require('path')
-const publicDirectoryPath = path.join(__dirname, './public')
+const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.json());
 app.use(express.static(publicDirectoryPath))
 // Additional requires/configs:
